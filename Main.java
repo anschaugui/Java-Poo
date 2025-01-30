524f680b0c02bcf3f0500bcedc906c8de0203c34
Guilherme Anschau <anschau.gui@gmail.com>
2025-01-30 08:32:09 -0300

    concatenação com java

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+       String product1 = "Computer";
+       String product2 = "Office Desk";
+
+       int age = 30;
+       int code =5290;
+       char gender = 'F';
+
+       double price1 = 2100.0;
+       double price2 = 650.0;
+       double measure = 53.234567;
+
+        System.out.println("products");
+
+        System.out.println(product1 + " which price is " + price1 + " Office desk, which price is " + price2);
+
+        System.out.println(" record: " + age + " years old, code " + code + " and gender: " + gender);
     }
 
 }
